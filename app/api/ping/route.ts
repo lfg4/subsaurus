043b409878f05b81ce84b989b4a7136930b74a6e
Laura Fernández García <laurafergar4@gmail.com>
2026-01-14 10:28:51 +0100

    Refactor ping route to simplify response handling
    - Removed the service call and error handling from the GET method in the ping route.
    - Updated the response to return a static JSON object with a 'pong' message, streamlining the code.

--- app/api/ping/route.ts
+++ app/api/ping/route.ts
@@ -1,17 +1,9 @@
-import { getPingService } from '@/src/container';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  try {
-    const service = getPingService();
-    const response = await service.handlePing();
+   
+    const response = {text: 'pong'};
     return NextResponse.json(response);
-  } catch (_error) {
-    console.log(_error)
-    return NextResponse.json(
-      { status: 'error', message: 'Internal server error' },
-      { status: 500 }
-    );
-  }
+ 
 }
 
